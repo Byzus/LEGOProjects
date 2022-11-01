20beedd0dbf626df67bdcfe4b0e954d8b0c68313
Byzus <byzus@proton.me>
2022-11-01 10:05:15 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
+from spike.control import wait_for_seconds, wait_until, Timer
+from spike import MotorPair
+from math import *
+
+hub = PrimeHub()
+
+hub.light.off()
+hub.speaker.beep()
+
+motor_pair = MotorPair('B', 'A')
